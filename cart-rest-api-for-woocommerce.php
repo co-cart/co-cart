cdbbebb704aaf9146944ff4bab20581706b20a64
Sébastien Dumont <mailme@sebastiendumont.com>
2018-11-15 16:49:14 +0100

    Updated WC tested up to

--- cart-rest-api-for-woocommerce.php
+++ cart-rest-api-for-woocommerce.php
@@ -12,7 +12,7 @@
  * Requires at least: 4.4
  * Tested up to: 4.9.8
  * WC requires at least: 3.2.0
- * WC tested up to: 3.4.4
+ * WC tested up to: 3.5.1
  *
  * Copyright: © 2018 Sébastien Dumont, (mailme@sebastiendumont.com)
  *
