18910cf3dadc14b98a42c2f3b4ff089bd7658592
Sébastien Dumont <mailme@sebastiendumont.com>
2019-06-18 16:10:27 +0200

    Fixed $this error with getting cart to check if item is in cart

--- includes/api/class-cocart-controller.php
+++ includes/api/class-cocart-controller.php
@@ -436,7 +436,7 @@ class CoCart_API_Controller {
 	 */
 	public function find_product_in_cart( $cart_id = false ) {
 		if ( false !== $cart_id ) {
-			if ( is_array( $this->get_cart() ) && null !== $this->get_cart( array(), $cart_id ) ) {
+			if ( is_array( WC()->cart->get_cart() ) && null !== WC()->cart->get_cart( array(), $cart_id ) ) {
 				return $cart_id;
 			}
 		}
