50db498bbd91fc631011e749ac45ddddb5efbcbb
Sébastien Dumont <mailme@sebastiendumont.com>
2019-08-25 23:19:46 +0200

    Tweaked upgrade link

--- includes/admin/class-cocart-admin-action-links.php
+++ includes/admin/class-cocart-admin-action-links.php
@@ -5,7 +5,7 @@
  * Adds links to CoCart on the plugins page.
  *
  * @since    1.2.0
- * @version  2.0.0
+ * @version  2.0.4
  * @author   Sébastien Dumont
  * @category Admin
  * @package  CoCart/Admin
@@ -57,7 +57,7 @@ if ( ! class_exists( 'CoCart_Admin_Action_Links' ) ) {
 		 *
 		 * @access  public
 		 * @since   2.0.0
-		 * @version 2.0.1
+		 * @version 2.0.4
 		 * @param   array  $metadata An array of the plugin's metadata.
 		 * @param   string $file     Path to the plugin file.
 		 * @param   array  $data     Plugin Information
@@ -86,10 +86,7 @@ if ( ! class_exists( 'CoCart_Admin_Action_Links' ) ) {
 				if ( ! CoCart_Admin::is_cocart_pro_installed() ) {
 					$store_url = add_query_arg( $campaign_args, COCART_STORE_URL . 'pricing-lite/' );
 
-					// If it has been a week or more since activating the plugin then offer to upgrade.
-					if ( ( intval( time() - $install_date ) ) > WEEK_IN_SECONDS ) {
-						$row_meta['upgrade'] = sprintf( '<a href="%1$s" aria-label="' . sprintf( esc_attr__( 'Upgrade to %s', 'cart-rest-api-for-woocommerce' ), 'CoCart Pro' ) . '" target="_blank" style="color: #39b54a; font-weight: 700;">%2$s</a>', esc_url( $store_url ), esc_attr__( 'Upgrade to Pro', 'cart-rest-api-for-woocommerce' ) );
-					}
+					$row_meta['upgrade'] = sprintf( '<a href="%1$s" aria-label="' . sprintf( esc_attr__( 'Upgrade to %s', 'cart-rest-api-for-woocommerce' ), 'CoCart Pro' ) . '" target="_blank" style="color: #c00; font-weight: 700;">%2$s</a>', esc_url( $store_url ), esc_attr__( 'Upgrade to Pro', 'cart-rest-api-for-woocommerce' ) );
 				}
 
 				$metadata = array_merge( $metadata, $row_meta );
