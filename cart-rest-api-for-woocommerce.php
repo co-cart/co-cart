46611f6b754d4656f1d4db1c488c9debd897cbf2
Sébastien Dumont <mailme@sebastiendumont.com>
2020-03-05 16:45:28 +0100

    Bumped WC version tested up to

--- cart-rest-api-for-woocommerce.php
+++ cart-rest-api-for-woocommerce.php
@@ -10,7 +10,7 @@
  * Domain Path: /languages/
  *
  * WC requires at least: 3.6.0
- * WC tested up to: 3.8.0
+ * WC tested up to: 3.9.3
  *
  * Copyright: © 2020 Sébastien Dumont, (mailme@sebastiendumont.com)
  *
