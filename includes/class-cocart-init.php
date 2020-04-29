ff8c033e5a192654374aafbc1ce2ed85bdf87ee7
Sébastien Dumont <mailme@sebastiendumont.com>
2020-04-29 16:31:51 +0200

    Tweaked order of loading cart

--- includes/class-cocart-init.php
+++ includes/class-cocart-init.php
@@ -276,24 +276,25 @@ class CoCart_Rest_API {
 		if ( is_numeric( $customer_id ) && $customer_id < 1 ) {
 			$cookie = WC()->session->get_cart_cookie();
 
+			// If cookie exists then return customer ID from it.
 			if ( $cookie ) {
 				$customer_id = $cookie[0];
+			}
 
-				// Get cart for customer.
-				$cart = WC()->session->get_cart( $customer_id );
+			// Get cart for customer.
+			$cart = WC()->session->get_cart( $customer_id );
 
-				// Set cart for customer if not empty.
-				if ( ! empty( $cart ) ) {
-					WC()->session->set( 'cart', maybe_unserialize( $cart[ 'cart' ] ) );
-					WC()->session->set( 'cart_totals', maybe_unserialize( $cart[ 'cart_totals' ] ) );
-					WC()->session->set( 'applied_coupons', maybe_unserialize( $cart[ 'applied_coupons' ] ) );
-					WC()->session->set( 'coupon_discount_totals', maybe_unserialize( $cart[ 'coupon_discount_totals' ] ) );
-					WC()->session->set( 'coupon_discount_tax_totals', maybe_unserialize( $cart[ 'coupon_discount_tax_totals' ] ) );
-					WC()->session->set( 'removed_cart_contents', maybe_unserialize( $cart[ 'removed_cart_contents' ] ) );
+			// Set cart for customer if not empty.
+			if ( ! empty( $cart ) ) {
+				WC()->session->set( 'cart', maybe_unserialize( $cart[ 'cart' ] ) );
+				WC()->session->set( 'cart_totals', maybe_unserialize( $cart[ 'cart_totals' ] ) );
+				WC()->session->set( 'applied_coupons', maybe_unserialize( $cart[ 'applied_coupons' ] ) );
+				WC()->session->set( 'coupon_discount_totals', maybe_unserialize( $cart[ 'coupon_discount_totals' ] ) );
+				WC()->session->set( 'coupon_discount_tax_totals', maybe_unserialize( $cart[ 'coupon_discount_tax_totals' ] ) );
+				WC()->session->set( 'removed_cart_contents', maybe_unserialize( $cart[ 'removed_cart_contents' ] ) );
 
-					if ( ! empty( $cart['cart_fees'] ) ) {
-						WC()->session->set( 'cart_fees', maybe_unserialize( $cart[ 'cart_fees' ] ) );
-					}
+				if ( ! empty( $cart['cart_fees'] ) ) {
+					WC()->session->set( 'cart_fees', maybe_unserialize( $cart[ 'cart_fees' ] ) );
 				}
 			}
 		}
