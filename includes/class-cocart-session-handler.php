d1bd91c66fe102db681ce6252afac5184b681b6d
Sébastien Dumont <mailme@sebastiendumont.com>
2020-05-03 01:54:54 +0200

    Changed priority order for loading cart

--- includes/class-cocart-session-handler.php
+++ includes/class-cocart-session-handler.php
@@ -90,14 +90,6 @@ class CoCart_Session_Handler extends WC_Session {
 
 		// Current user ID. If user is NOT logged in then the customer is a guest.
 		$current_user_id = strval( get_current_user_id() );
-		
-		// No cookie, then check if we requested to load a specific cart.
-		if ( isset( $_REQUEST['id'] ) ) {
-			$cart_id = $_REQUEST['id'];
-
-			// Set customer ID in session.
-			$this->_customer_id = $cart_id;
-		}
 
 		// Does a cookie exist?
 		if ( $cookie ) {
@@ -106,6 +98,14 @@ class CoCart_Session_Handler extends WC_Session {
 			$this->_cart_expiration = $cookie[1];
 			$this->_cart_expiring   = $cookie[2];
 			$this->_has_cookie      = true;
+		}
+
+		// No cookie, then check if we requested to load a specific cart.
+		if ( isset( $_REQUEST['id'] ) ) {
+			$cart_id = $_REQUEST['id'];
+
+			// Set customer ID in session.
+			$this->_customer_id = $cart_id;
 		}
 
 		// If cart retrieved then update cart.
