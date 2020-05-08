7b7e57eac4d01a6822c3f651387919fd15bc79a9
Sébastien Dumont <mailme@sebastiendumont.com>
2020-05-08 13:55:58 +0200

    Removed unused variable for database

--- includes/class-cocart-install.php
+++ includes/class-cocart-install.php
@@ -194,13 +194,6 @@ if ( ! class_exists( 'CoCart_Install' ) ) {
 				$collate = $wpdb->get_charset_collate();
 			}
 
-			/**
-			 * Indexes have a maximum size of 767 bytes. Historically, don't need to be concerned about that.
-			 * As of WP 4.2, however, they moved to utf8mb4, which uses 4 bytes per character. This means that an index which
-			 * used to have room for floor(767/3) = 255 characters, now only has room for floor(767/4) = 191 characters.
-			 */
-			$max_index_length = 191;
-
 			// Queries
 			$tables = 
 				"CREATE TABLE {$wpdb->prefix}cocart_carts (
