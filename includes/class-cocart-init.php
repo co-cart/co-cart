79138e5550a37cff2c8a3a0e6ec3679f7beb0a4c
mattdabell <mattdabell@live.com>
2020-08-17 11:45:21 +0100

    Fix CORS requests by replacing wildcard * with specific origin in Access-Control-Allow-Origin

--- includes/class-cocart-init.php
+++ includes/class-cocart-init.php
@@ -376,7 +376,7 @@ class CoCart_Rest_API {
 	 * @return bool
 	 */
 	public function cors_headers( $served, $result, $request, $server ) {
-		header( 'Access-Control-Allow-Origin: *' );
+        header( 'Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN'] );
 		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE' );
 		header( 'Access-Control-Allow-Credentials: true' );
 		header( 'Access-Control-Allow-Headers: Authorization, Content-Type, X-Requested-With' );
