c4c9336307c7dfae0a76ae05b3b93f8c310238c0
Sébastien Dumont <mailme@sebastiendumont.com>
2020-10-05 17:31:11 +0200

    Bumped version and updated POT

--- includes/class-cocart.php
+++ includes/class-cocart.php
@@ -26,7 +26,7 @@ final class CoCart {
 	 * @access public
 	 * @static
 	 */
-	public static $version = '2.7.0-rc.2';
+	public static $version = '2.7.0-rc.3';
 
 	/**
 	 * Required WordPress Version
