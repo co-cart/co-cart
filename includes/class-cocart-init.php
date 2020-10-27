5ab6ac4f46ea58117afab8423de73c0047c87c91
Sébastien Dumont <mailme@sebastiendumont.com>
2020-10-27 20:33:12 +0100

    Use a loose comparison `!=` instead

--- includes/class-cocart-init.php
+++ includes/class-cocart-init.php
@@ -215,7 +215,7 @@ class CoCart_Rest_API {
 			$customer_id = $cookie[0];
 
 			// If the user is logged in and does not match ID in cookie then user has switched.
-			if ( $current_user_id !== $customer_id && $current_user_id !== 0 ) {
+			if ( $current_user_id !== $customer_id && $current_user_id != 0 ) {
 				CoCart_Logger::log( sprintf( __( 'User has changed! Was %1$s before and is now %2$s', 'cart-rest-api-for-woocommerce' ), $customer_id, $current_user_id ), 'info' );
 
 				return true;
