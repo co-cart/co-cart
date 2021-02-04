dc8b27669b396eb9b8c4dd6a6f3259551af48a73
Sébastien Dumont <mailme@sebastiendumont.com>
2021-02-04 17:23:52 +0100

    Added "CoCart" browse section to plugin install

--- includes/admin/class-cocart-admin-plugin-search.php
+++ includes/admin/class-cocart-admin-plugin-search.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Includes cards in the plugin search results when users 
- * enter terms that match CoCart add-ons.
+ * enter terms that match CoCart add-ons or view all add-ons.
  *
  * @author   Sébastien Dumont
  * @category Admin
@@ -34,12 +34,114 @@ if ( ! class_exists( 'CoCart_Plugin_Search' ) ) {
 		 * @param object $screen WP Screen object.
 		 */
 		public function start( $screen ) {
-			if ( 'plugin-install' === $screen->base && ( ! isset( $_GET['paged'] ) || 1 === intval( $_GET['paged'] ) ) ) {
+			if ( 'plugin-install' === $screen->base ) {
+				// Filters below inject plugin suggestion.
 				add_action( 'admin_enqueue_scripts', array( $this, 'load_plugins_search_script' ) );
 				add_filter( 'plugins_api_result', array( $this, 'inject_cocart_suggestion' ), 10, 3 );
 				add_filter( 'plugin_install_action_links', array( $this, 'inset_related_links' ), 10, 2 );
+
+				// Filters below are for CoCarts own plugin section.
+				add_filter( 'plugins_api_result', array( $this, 'cocart_plugins' ), 10, 3 );
+				add_filter( 'install_plugins_tabs', array( $this, 'plugins_tab' ) );
+				add_filter( 'install_plugins_table_api_args_cocart', array( $this, 'plugin_list_args' ) );
+				add_action( 'install_plugins_cocart', array( $this, 'cocart_plugin_dashboard' ) );
 			}
 		}
+
+		/**
+		 * Add CoCart plugin tab.
+		 *
+		 * @access public
+		 * @param  array $tabs Default plugin tabs.
+		 * @return array $tabs Altered plugin tabs.
+		 */
+		public function plugins_tab( $tabs ) {
+			return array_merge(
+				$tabs,
+				array(
+					'cocart' => 'CoCart',
+				)
+			);
+		} // END plugins_tab()
+
+		/**
+		 * Set CoCart tab args.
+		 *
+		 * This is so we can trigger "plugins_api_result" 
+		 * action hook to return our results.
+		 *
+		 * @access public
+		 * @param  object $args
+		 * @return object $args
+		 */
+		public function plugin_list_args( $args ) {
+			$installed_plugins = self::get_installed_plugins();
+
+			$per_page = 30;
+
+			$args = array(
+				'page'     => isset( $GET['paged'] ) ? max(0, intval( $_GET['paged'] -1 ) * $per_page) : 0,
+				'per_page' => $per_page,
+				'author'   => 'cocartforwc',
+				'installed_plugins' => array_keys( $installed_plugins ),
+				// Send the locale to the API so it can provide context-sensitive results.
+				'locale'   => get_user_locale(),
+			);
+
+			return $args;
+		} // END plugin_list_args()
+
+		/**
+		 * Return the list of known plugins.
+		 *
+		 * Uses the transient data from the updates API to determine the known
+		 * installed plugins.
+		 *
+		 * @access protected
+		 * @return array
+		 */
+		protected function get_installed_plugins() {
+			$plugins = array();
+
+			$plugin_info = get_site_transient( 'update_plugins' );
+
+			if ( isset( $plugin_info->no_update ) ) {
+				foreach ( $plugin_info->no_update as $plugin ) {
+					$plugin->upgrade          = false;
+					$plugins[ $plugin->slug ] = $plugin;
+				}
+			}
+
+			if ( isset( $plugin_info->response ) ) {
+				foreach ( $plugin_info->response as $plugin ) {
+					$plugin->upgrade          = true;
+					$plugins[ $plugin->slug ] = $plugin;
+				}
+			}
+
+			return $plugins;
+		} // END get_installed_plugins()
+
+		/**
+		 * Displays our own plugin dashboard on the plugin install page.
+		 *
+		 * @access public
+		 */
+		public function cocart_plugin_dashboard() {
+			?>
+			<p>
+				<?php
+				printf(
+					/* translators: %1$s: https://cocart.xyz/add-ons/, %2$s: https://cocart.xyz/woocommerce-extensions/ */
+					__( 'These plugins extend and expand the functionality of CoCart. You may learn more about each of the <a href="%1$s" target="_blank">CoCart add-ons</a> and <a href="%2$s" target="_blank">WooCommerce extensions</a> from CoCart.xyz' ),
+					esc_url( 'https://cocart.xyz/add-ons/' ),
+					esc_url( 'https://cocart.xyz/woocommerce-extensions/' )
+				);
+				?>
+			</p>
+
+			<?php display_plugins_table();
+		} // END cocart_plugin_dashboard()
 
 		/**
 		 * Load the search scripts and CSS for Plugin Search Suggestion.
@@ -264,6 +366,70 @@ if ( ! class_exists( 'CoCart_Plugin_Search' ) ) {
 			// Return search results.
 			return $result;
 		} // END inject_cocart_suggestion()
+
+		/**
+		 * Filter plugin fetching API results to return CoCart add-ons.
+		 *
+		 * @access public
+		 * @param  object|WP_Error $result Response object or WP_Error.
+		 * @param  string          $action The type of information being requested from the Plugin Install API.
+		 * @param  object          $args   Plugin API arguments.
+		 * @return array Updated array of results
+		 */
+		public function cocart_plugins( $result, $action, $args ) {
+			// If we are not browsing just CoCart then return results.
+			if ( 'cocartforwc' !== $args->author ) {
+				return $result;
+			}
+
+			// Get CoCart plugin data.
+			$inject = (array) self::get_cocart_plugin_data();
+
+			// Return current results if failed to get plugin data.
+			if ( is_wp_error( $inject ) ) {
+				return $result;
+			}
+
+			$suggestions = self::get_suggestions();
+
+			// Get each add-on and see if we should suggest it to the user.
+			foreach( $suggestions as $slug => $data ) {
+				$inject_data = array(
+					'name'              => empty( $data['third_party'] ) ? sprintf( esc_html__( '%1$s Add-on', 'cart-rest-api-for-woocommerce' ), $data['name'] ) : $data['name'],
+					'slug'              => empty( $data['third_party'] ) ? 'cocart-' . $data['plugin'] : $data['plugin'],
+					'plugin'            => $data['plugin'],
+					'version'           => '',
+					'author'            => ! empty( $data['author'] ) ? esc_html( $data['author'] ) : 'CoCart',
+					'author_profile'    => 'https://cocart.xyz',
+					'requires'          => $inject['requires'],
+					'tested'            => $inject['tested'],
+					'requires_php'      => $inject['requires_php'],
+					'rating'            => $inject['rating'],
+					'num_ratings'       => $inject['num_ratings'],
+					'active_installs'   => $inject['active_installs'],
+					'last_updated'      => $inject['last_updated'],
+					'short_description' => $data['short_description'],
+					'download_link'     => '',
+					'icons'             => $inject['icons'],
+					'logo'              => array(
+						'1x'  => esc_url( $data['logo'] ),
+						'2x'  => esc_url( $data['logo'] ),
+						'svg' => esc_url( $data['logo'] ),
+					),
+					'purchase'          => ! empty( $data['purchase'] ) ? esc_url( $data['purchase'] ): esc_url( 'https://cocart.xyz/pro/#pricing' ),
+					'learn_more'        => esc_url( $data['learn_more'] ),
+					'third_party'       => $data['third_party']
+				);
+
+				// Override card icon.
+				$inject_data['icons'] = $inject_data['logo'];
+
+				array_unshift( $result->plugins, $inject_data );
+			} // END foreach add-on
+
+			// Return search results.
+			return $result;
+		} // END cocart_plugins()
 
 		/**
 		 * Take a raw search query and return something a bit more standardized and
