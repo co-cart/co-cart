1e20b1c0856b882cb4a2d0363f36fba910fda73c
CodeBreaker <mailme@sebastiendumont.com>
2021-02-07 21:07:32 +0100

    Fix switch

--- includes/admin/class-cocart-admin.php
+++ includes/admin/class-cocart-admin.php
@@ -63,7 +63,7 @@ if ( ! class_exists( 'CoCart_Admin' ) ) {
 					include_once COCART_ABSPATH . 'includes/admin/class-cocart-admin-action-links.php';         // Plugin Action Links
 					include_once COCART_ABSPATH . 'includes/admin/class-cocart-admin-plugin-screen-update.php'; // Plugin Update
 					break;
-				case 'plugin-install'
+				case 'plugin-install':
 					include_once COCART_ABSPATH . 'includes/admin/class-cocart-admin-plugin-search.php';        // Plugin Search
 					break;
 			}
