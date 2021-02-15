bf22618fb2dde7295948670876bdfbe247063356
CodeBreaker <mailme@sebastiendumont.com>
2021-02-15 20:07:11 +0100

    No longer need to check if response was an error

--- includes/api/class-cocart-add-item-controller.php
+++ includes/api/class-cocart-add-item-controller.php
@@ -119,11 +119,6 @@ class CoCart_Add_Item_v2_Controller extends CoCart_Add_Item_Controller {
 
 		$product_to_add = $controller->validate_product( $product_id, $quantity, 0, array(), $item_data, 'simple', $request );
 
-		// If validation failed then return the error response.
-		/*if ( is_wp_error( $product_to_add ) ) {
-			return $product_to_add;
-		}*/
-
 		// Add item to cart once validation is passed.
 		$item_added = $this->add_item_to_cart( $product_to_add );
 
@@ -147,11 +142,6 @@ class CoCart_Add_Item_v2_Controller extends CoCart_Add_Item_Controller {
 		$controller = new CoCart_Cart_V2_Controller();
 
 		$product_to_add = $controller->validate_product( $product_id, $quantity, $deprecated, $variation, $item_data, 'variable', $request );
-
-		// If validation failed then return the error response.
-		/*if ( is_wp_error( $product_to_add ) ) {
-			return $product_to_add;
-		}*/
 
 		// Add item to cart once validation is passed.
 		$item_added = $this->add_item_to_cart( $product_to_add );
