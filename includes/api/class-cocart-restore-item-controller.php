bb59e0b471672e4b118df544519e1b7072713914
CodeBreaker <mailme@sebastiendumont.com>
2021-02-18 14:34:39 +0100

    📦 NEW: Restore item API

--- includes/api/class-cocart-restore-item-controller.php
+++ includes/api/class-cocart-restore-item-controller.php
@@ -19,8 +19,9 @@ if ( ! defined( 'ABSPATH' ) ) {
  * REST API Restore Item controller class.
  *
  * @package CoCart\API
+ * @extends CoCart_Item_v2_Controller
  */
-class CoCart_Restore_Item_v2_Controller extends CoCart_Item_Controller {
+class CoCart_Restore_Item_v2_Controller extends CoCart_Item_v2_Controller {
 
 	/**
 	 * Endpoint namespace.
@@ -42,11 +43,11 @@ class CoCart_Restore_Item_v2_Controller extends CoCart_Item_Controller {
 	 * @access public
 	 */
 	public function register_routes() {
-		// Restore Item - cocart/v2/restore-item (GET)
+		// Restore Item - cocart/v2/restore-item (PUT)
 		register_rest_route( $this->namespace, '/' . $this->rest_base, array(
 			'args' => $this->get_collection_params(),
 			array(
-				'methods'             => WP_REST_Server::READABLE,
+				'methods'             => WP_REST_Server::EDITABLE,
 				'callback'            => array( $this, 'restore_item' ),
 				'permission_callback' => '__return_true',
 			),
@@ -54,7 +55,77 @@ class CoCart_Restore_Item_v2_Controller extends CoCart_Item_Controller {
 	} // register_routes()
 
 	/**
-	 * Get the query params for item.
+	 * Restore Item in Cart.
+	 *
+	 * @throws CoCart_Data_Exception Exception if invalid data is detected.
+	 *
+	 * @access  public
+	 * @since   1.0.0
+	 * @version 3.0.0
+	 * @param   WP_REST_Request $request Full details about the request.
+	 * @return  WP_REST_Response
+	 */
+	public function restore_item( $request = array() ) {
+		try {
+			$item_key = ! isset( $request['item_key'] ) ? '0' : sanitize_text_field( wp_unslash( wc_clean( $request['item_key'] ) ) );
+
+			if ( 0 === $item_key || $item_key < 0 ) {
+				$message = __( 'Cart item key is required!', 'cart-rest-api-for-woocommerce' );
+
+				/**
+				 * Filters message about cart item key required.
+				 *
+				 * @since 2.1.0
+				 * @param string $message Message.
+				 */
+				$message = apply_filters( 'cocart_cart_item_key_required_message', $message, 'update' );
+
+				throw new CoCart_Data_Exception( 'cocart_cart_item_key_required', $message, 404 );
+			}
+
+			$controller = new CoCart_Cart_V2_Controller();
+
+			if ( $controller->get_cart_instance()->restore_cart_item( $item_key ) ) {
+				$restored_item = $controller->get_cart_item( $item_key, 'restore' ); // Fetches the cart item data once it is restored.
+
+				do_action( 'cocart_item_restored', $restored_item );
+
+				/**
+				 * Calculates the cart totals now an item has been restored.
+				 *
+				 * @since 2.1.0
+				 */
+				$controller->get_cart_instance()->calculate_totals();
+
+				// Get cart contents.
+				$response = $controller->get_cart_contents( $request );
+
+				// Was it requested to return just the restored item?
+				if ( $request['return_item'] ) {
+					$response = $restored_item;
+				}
+
+				return CoCart_Response::get_response( $response, $this->namespace, $this->rest_base );
+			} else {
+				$message = __( 'Unable to restore item to the cart.', 'cart-rest-api-for-woocommerce' );
+
+				/**
+				 * Filters message about can not restore item.
+				 *
+				 * @since 2.1.0
+				 * @param string $message Message.
+				 */
+				$message = apply_filters( 'cocart_can_not_restore_item_message', $message );
+
+				throw new CoCart_Data_Exception( 'cocart_can_not_restore_item', $message, 403 );
+			}
+		} catch( CoCart_Data_Exception $e) {
+			return CoCart_Response::get_error_response( $e->getErrorCode(), $e->getMessage(), $e->getCode(), $e->getAdditionalData() );
+		}
+	} // END restore_item()
+
+	/**
+	 * Get the query params for restoring an item.
 	 *
 	 * @access public
 	 * @return array $params
@@ -67,8 +138,8 @@ class CoCart_Restore_Item_v2_Controller extends CoCart_Item_Controller {
 				'sanitize_callback' => 'sanitize_text_field',
 				'validate_callback' => 'rest_validate_request_arg',
 			),
-			'return_cart'   => array(
-				'description'       => __( 'Returns the whole cart to reduce API requests.', 'cart-rest-api-for-woocommerce' ),
+			'return_item'   => array(
+				'description' => __( 'Returns the item details once restored.', 'cart-rest-api-for-woocommerce' ),
 				'default'           => false,
 				'type'              => 'boolean',
 				'validate_callback' => 'rest_validate_request_arg',
