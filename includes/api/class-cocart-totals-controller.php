dbe8f09465244b4b30554db523e08f1de99dd993
CodeBreaker <mailme@sebastiendumont.com>
2021-02-25 01:01:31 +0100

    📦 NEW: API for getting the cart totals

--- includes/api/class-cocart-totals-controller.php
+++ includes/api/class-cocart-totals-controller.php
@@ -63,4 +63,67 @@ class CoCart_Totals_v2_Controller extends CoCart_Totals_Controller {
 		);
 	} // register_routes()
 
+	/**
+	 * Returns all calculated totals.
+	 *
+	 * @throws CoCart_Data_Exception Exception if invalid data is detected.
+	 *
+	 * @access  public
+	 * @static
+	 * @since   1.0.0
+	 * @version 3.0.0
+	 * @param   WP_REST_Request $request - Full details about the request.
+	 * @return  WP_REST_Response
+	 */
+	public function get_totals( $request = array() ) {
+		try {
+			$pre_formatted = isset( $request['html'] ) ? $request['html'] : false;
+
+			$controller = new CoCart_Cart_V2_Controller();
+
+			$totals = array();
+			$totals_calculated = false;
+
+			if ( ! empty( $controller->get_cart_instance()->totals ) ) {
+				$totals            = $controller->get_cart_instance()->get_totals();
+				$totals_calculated = true;
+			}
+
+			$ignore_convert = array(
+				'shipping_taxes',
+				'cart_contents_taxes',
+				'fee_taxes',
+			);
+
+			if ( ! $totals_calculated ) {
+				$message = esc_html__( 'This cart either has no items or was not calculated.', 'cart-rest-api-for-woocommerce' );
+
+				throw new CoCart_Data_Exception( 'cocart_cart_totals_empty', $message, 404 );
+			}
+
+			// Was it requested to have the totals preformatted?
+			if ( $pre_formatted ) {
+				$new_totals = array();
+
+				foreach ( $totals as $type => $total ) {
+					if ( in_array( $type, $ignore_convert ) ) {
+						$new_totals[ $type ] = $total;
+					} else {
+						if ( is_string( $total ) ) {
+							$new_totals[ $type ] = html_entity_decode( strip_tags( wc_price( $total ) ) );
+						} else {
+							$new_totals[ $type ] = html_entity_decode( strip_tags( wc_price( strval( $total ) ) ) );
+						}
+					}
+				}
+
+				$totals = $new_totals;
+			}
+
+			return CoCart_Response::get_response( $totals, $this->namespace, $this->rest_base );
+		} catch ( CoCart_Data_Exception $e ) {
+			return CoCart_Response::get_error_response( $e->getErrorCode(), $e->getMessage(), $e->getCode(), $e->getAdditionalData() );
+		}
+	} // END get_totals()
+
 } // END class
