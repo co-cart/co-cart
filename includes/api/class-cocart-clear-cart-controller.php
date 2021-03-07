a9e16c141e20b6278d0c3bc2d863d9d30dff5945
CodeBreaker <mailme@sebastiendumont.com>
2021-03-07 17:16:31 +0100

    📦 NEW: Clear cart endpoint

--- includes/api/class-cocart-clear-cart-controller.php
+++ includes/api/class-cocart-clear-cart-controller.php
@@ -54,4 +54,54 @@ class CoCart_Clear_Cart_v2_Controller extends CoCart_Clear_Cart_Controller {
 		);
 	} // register_routes()
 
+	/**
+	 * Clears the cart.
+	 *
+	 * @throws CoCart_Data_Exception Exception if invalid data is detected.
+	 *
+	 * @access  public
+	 * @since   1.0.0
+	 * @version 3.0.0
+	 * @return  WP_REST_Response
+	 */
+	public function clear_cart() {
+		try {
+			$controller = new CoCart_Cart_V2_Controller();
+
+			$controller->get_cart_instance()->empty_cart();
+
+			if ( $controller->get_cart_instance()->is_empty() ) {
+				do_action( 'cocart_cart_cleared' );
+
+				$message = __( 'Cart is cleared.', 'cart-rest-api-for-woocommerce' );
+
+				CoCart_Logger::log( $message, 'notice' );
+
+				/**
+				 * Filters message about the cart being cleared.
+				 *
+				 * @since 2.1.0
+				 * @param string $message Message.
+				 */
+				$message = apply_filters( 'cocart_cart_cleared_message', $message );
+
+				return CoCart_Response::get_response( $message, $this->namespace, $this->rest_base );
+			} else {
+				$message = __( 'Clearing the cart failed!', 'cart-rest-api-for-woocommerce' );
+
+				/**
+				 * Filters message about the cart failing to clear.
+				 *
+				 * @since 2.1.0
+				 * @param string $message Message.
+				 */
+				$message = apply_filters( 'cocart_clear_cart_failed_message', $message );
+
+				throw new CoCart_Data_Exception( 'cocart_clear_cart_failed', $message, 404 );
+			}
+		} catch ( CoCart_Data_Exception $e ) {
+			return CoCart_Response::get_error_response( $e->getErrorCode(), $e->getMessage(), $e->getCode(), $e->getAdditionalData() );
+		}
+	} // END clear_cart()
+
 } // END class
