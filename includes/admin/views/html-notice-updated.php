e5b99a8a40e0702fc89e4da546ecdf4c127a9004
CodeBreaker <mailme@sebastiendumont.com>
2021-03-18 14:19:39 +0100

    Fixed layout for updated notice

--- includes/admin/views/html-notice-updated.php
+++ includes/admin/views/html-notice-updated.php
@@ -16,13 +16,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="notice notice-info cocart-notice">
 	<div class="cocart-notice-inner">
+		<div class="cocart-notice-icon">
+			<img src="<?php echo COCART_URL_PATH . '/assets/images/logo.jpg'; ?>" alt="CoCart Logo" />
+		</div>
+
 		<div class="cocart-notice-content">
 			<h3><?php esc_html_e( 'Database updated', 'cart-rest-api-for-woocommerce' ); ?></h3>
 			<p><?php echo sprintf( esc_html__( '%s database update complete. Thank you for updating to the latest version!', 'cart-rest-api-for-woocommerce' ), 'CoCart' ); ?></p>
+		</div>
 
-			<div class="cocart-action">
-				<a class="button button-primary cocart-button" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'cocart-hide-notice', 'update_db', remove_query_arg( 'do_update_cocart', CoCart_Helpers::cocart_get_current_admin_url() ) ), 'cocart_hide_notices_nonce', '_cocart_notice_nonce' ) ); ?>"><?php esc_html_e( 'Dismiss', 'cart-rest-api-for-woocommerce' ); ?></a>
-			</div>
+		<div class="cocart-action">
+			<a class="button button-primary cocart-button" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'cocart-hide-notice', 'update_db', remove_query_arg( 'do_update_cocart', CoCart_Helpers::cocart_get_current_admin_url() ) ), 'cocart_hide_notices_nonce', '_cocart_notice_nonce' ) ); ?>"><?php esc_html_e( 'Dismiss', 'cart-rest-api-for-woocommerce' ); ?></a>
 		</div>
 	</div>
 </div>
