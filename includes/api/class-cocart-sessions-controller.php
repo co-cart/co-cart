12e294bcd644fd0d219096f25f2b9bc33b39b994
CodeBreaker <mailme@sebastiendumont.com>
2021-04-10 01:29:29 +0200

    Added link to individual session API

--- includes/api/class-cocart-sessions-controller.php
+++ includes/api/class-cocart-sessions-controller.php
@@ -122,6 +122,7 @@ class CoCart_Sessions_V2_Controller extends CoCart_Cart_V2_Controller {
 					'cart_created'    => date( 'm/d/Y H:i:s', $cart['cart_created'] ),
 					'cart_expiry'     => date( 'm/d/Y H:i:s', $cart['cart_expiry'] ),
 					'cart_source'     => $cart_source,
+					'link'            => rest_url( sprintf( '/%s/%s', $this->namespace, 'session/' . $cart['cart_key'] ) )
 				);
 			}
 
