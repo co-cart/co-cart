c17666fc1a675566a2b3ba86b2b8e8b489088576
CodeBreaker <mailme@sebastiendumont.com>
2021-06-03 16:39:03 +0200

    Added ability to get product by SKU

--- includes/api/products/class-cocart-products-controller.php
+++ includes/api/products/class-cocart-products-controller.php
@@ -30,4 +30,83 @@ class CoCart_Products_V2_Controller extends CoCart_Products_Controller {
 	 */
 	protected $namespace = 'cocart/v2';
 
+	/**
+	 * Register the routes for products.
+	 *
+	 * @access public
+	 */
+	public function register_routes() {
+		// Get a single product by ID - cocart/v2/products/32 (GET)
+		// Get a single product by SKU - cocart/v2/products/woo-vneck-tee (GET)
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/(?P<id>[\w-]+)',
+			array(
+				'args'   => array(
+					'id' => array(
+						'description' => __( 'Unique identifier for the product.', 'cart-rest-api-for-woocommerce' ),
+						'type'        => 'string',
+					),
+				),
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_item' ),
+					'args'                => array(
+						'context' => $this->get_context_param(
+							array(
+								'default' => 'view',
+							)
+						),
+					),
+					'permission_callback' => '__return_true',
+				),
+				'schema' => array( $this, 'get_public_item_schema' ),
+			)
+		);
+	}
+
+	/**
+	 * Get a single item.
+	 *
+	 * @throws  CoCart_Data_Exception Exception if invalid data is detected.
+	 *
+	 * @access public
+	 * @param  WP_REST_Request $request Full details about the request.
+	 * @return WP_REST_Response
+	 */
+	public function get_item( $request ) {
+		try {
+			$product_id = ! isset( $request['id'] ) ? 0 : wc_clean( wp_unslash( $request['id'] ) );
+
+			// If the product ID was used by a SKU ID, then look up the product ID and return it.
+			if ( ! is_numeric( $product_id ) ) {
+				$product_id_by_sku = wc_get_product_id_by_sku( $product_id );
+
+				if ( ! empty( $product_id_by_sku ) && $product_id_by_sku > 0 ) {
+					$product_id = $product_id_by_sku;
+				} else {
+					$message = __( 'Product does not exist! Check that you have submitted a product ID or SKU ID correctly for a product that exists.', 'cart-rest-api-for-woocommerce' );
+
+					throw new CoCart_Data_Exception( 'cocart_unknown_product_id', $message, 500 );
+				}
+
+				// Force product ID to be integer.
+				$product_id = (int) $product_id;
+			}
+
+			$object = $this->get_object( (int) $product_id );
+
+			if ( ! $object || 0 === $object->get_id() ) {
+				throw new CoCart_Data_Exception( 'cocart_' . $this->post_type . '_invalid_id', __( 'Invalid ID.', 'cart-rest-api-for-woocommerce' ), 404 );
+			}
+
+			$data     = $this->prepare_object_for_response( $object, $request );
+			$response = rest_ensure_response( $data );
+
+			return $response;
+		} catch ( CoCart_Data_Exception $e ) {
+			return CoCart_Response::get_error_response( $e->getErrorCode(), $e->getMessage(), $e->getCode(), $e->getAdditionalData() );
+		}
+	} // END get_item()
+
 } // END class
