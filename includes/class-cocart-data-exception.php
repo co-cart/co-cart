67823572564ed58dca80e56ef8fde301cb0a8957
seb86 <mailme@sebastiendumont.com>
2022-02-08 01:24:23 +0100

    👌 IMPROVE: Passed plugin slug used to identify error logs for.

--- includes/class-cocart-data-exception.php
+++ includes/class-cocart-data-exception.php
@@ -7,6 +7,7 @@
  * @author  Sébastien Dumont
  * @package CoCart\Classes
  * @since   3.0.0
+ * @version 3.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -37,17 +38,22 @@ class CoCart_Data_Exception extends Exception {
 	/**
 	 * Setup exception.
 	 *
-	 * @access public
-	 * @param  string $error_code       Machine-readable error code, e.g `cocart_invalid_product_id`.
-	 * @param  string $message          User-friendly translated error message, e.g. 'Product ID is invalid'.
-	 * @param  int    $http_status_code Proper HTTP status code to respond with, e.g. 400.
-	 * @param  array  $additional_data   Extra error data.
+	 * @access  public
+	 * @since   3.0.0  Introduced.
+	 * @since   3.1.0  Passed plugin slug used to identify error logs for.
+	 * @version 3.1.0
+	 * @param   string $error_code       Machine-readable error code, e.g `cocart_invalid_product_id`.
+	 * @param   string $message          User-friendly translated error message, e.g. 'Product ID is invalid'.
+	 * @param   int    $http_status_code Proper HTTP status code to respond with, e.g. 400.
+	 * @param   array  $additional_data  Extra error data.
 	 */
 	public function __construct( $error_code, $message, $http_status_code = 400, $additional_data = array() ) {
 		$this->error_code      = $error_code;
 		$this->additional_data = array_filter( (array) $additional_data );
 
-		CoCart_Logger::log( $message, 'error' );
+		$plugin = isset( $this->additional_data['plugin'] ) ? esc_html( $this->additional_data['plugin'] ) : '';
+
+		CoCart_Logger::log( $message, 'error', $plugin );
 
 		parent::__construct( $message, $http_status_code );
 	}
