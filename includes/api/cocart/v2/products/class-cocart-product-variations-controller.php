d42bab58d740c3c56549b01ed63b5339a4298de3
seb86 <mailme@sebastiendumont.com>
2022-02-13 01:16:58 +0100

    Finished variation route and added validation

--- includes/api/cocart/v2/products/class-cocart-product-variations-controller.php
+++ includes/api/cocart/v2/products/class-cocart-product-variations-controller.php
@@ -72,12 +72,39 @@ class CoCart_Product_Variations_V2_Controller extends CoCart_Product_Variations_
 							'type'        => 'integer',
 						),
 					),
-					'permission_callback' => '__return_true',
+					'permission_callback' => array( $this, 'validate_variation' ),
 				),
 				'schema' => array( $this, 'get_public_item_schema' ),
 			)
 		);
 	}
+
+	/**
+	 * Validate the variation exists and is part of the variable product.
+	 *
+	 * @access public
+	 * @param  WP_REST_Request $request Full details about the request.
+	 * @return WP_Error|bool
+	 */
+	public function validate_variation( $request ) {
+		$parent    = wc_get_product( (int) $request['product_id'] );
+		$variation = wc_get_product( (int) $request['id'] );
+
+		$variation_ids = $parent->get_children();
+
+		// Validate the variation product exists.
+		if ( ! $variation || 0 === $variation->get_id() ) {
+			return new WP_Error( 'cocart_' . $this->post_type . '_invalid_id', __( 'Invalid ID.', 'cart-rest-api-for-woocommerce' ), array( 'status' => 404 ) );
+		}
+
+		// Validate the variation requested to see if it is not one of the variations for the variable product.
+		if ( ! in_array( $variation->get_id(), $variation_ids ) ) {
+			return new WP_Error( 'cocart_' . $this->post_type . '_invalid_id', __( 'Invalid ID.', 'cart-rest-api-for-woocommerce' ), array( 'status' => 404 ) );
+		}
+
+		// Validation successful.
+		return true;
+	} // END validate_variation()
 
 	/**
 	 * Prepare a single variation output for response.
@@ -109,6 +136,21 @@ class CoCart_Product_Variations_V2_Controller extends CoCart_Product_Variations_
 		return apply_filters( "cocart_prepare_{$this->post_type}_object_v2", $response, $product, $request );
 	} // END prepare_object_for_response()
 
+	/**
+	 * Get a single item.
+	 *
+	 * @access public
+	 * @param  WP_REST_Request $request Full details about the request.
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function get_item( $request ) {
+		$product = wc_get_product( (int) $request['id'] );
+
+		$data     = $this->prepare_object_for_response( $product, $request );
+		$response = rest_ensure_response( $data );
+
+		return $response;
+	} // END get_item()
 
 	/**
 	 * Prepare links for the request.
