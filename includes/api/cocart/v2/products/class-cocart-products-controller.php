b2b5f4c4af4b04a77475c6dca892f12d98e2d3de
seb86 <mailme@sebastiendumont.com>
2022-03-16 15:51:55 +0100

    👌 IMPROVED: Moved products to it's own object and returned also pagination information.

--- includes/api/cocart/v2/products/class-cocart-products-controller.php
+++ includes/api/cocart/v2/products/class-cocart-products-controller.php
@@ -7,6 +7,7 @@
  * @author  Sébastien Dumont
  * @package CoCart\API\Products\v2
  * @since   3.1.0
+ * @version 3.2.0
  * @license GPL-2.0+
  */
 
@@ -78,6 +79,87 @@ class CoCart_Products_V2_Controller extends CoCart_Products_Controller {
 			)
 		);
 	}
+
+	/**
+	 * Get a collection of products.
+	 *
+	 * @access  public
+	 * @since   3.1.0 Introduced.
+	 * @since   3.2.0 Moved products to it's own object and returned also pagination information.
+	 * @version 3.2.0
+	 * @param   WP_REST_Request $request Full details about the request.
+	 * @return  WP_Error|WP_REST_Response
+	 */
+	public function get_items( $request ) {
+		$query_args    = $this->prepare_objects_query( $request );
+		$query_results = $this->get_objects( $query_args );
+
+		$objects = array();
+
+		foreach ( $query_results['objects'] as $object ) {
+			$data      = $this->prepare_object_for_response( $object, $request );
+			$objects[] = $this->prepare_response_for_collection( $data );
+		}
+
+		$page      = (int) $query_args['paged'];
+		$max_pages = $query_results['pages'];
+
+		$results = array(
+			'products'       => $objects,
+			'page'           => $page,
+			'total_pages'    => (int) $max_pages,
+			'total_products' => $query_results['total']
+		);
+		$response = rest_ensure_response( $results );
+		$response->header( 'X-WP-Total', $query_results['total'] );
+		$response->header( 'X-WP-TotalPages', (int) $max_pages );
+
+		$base          = $this->rest_base;
+		$attrib_prefix = '(?P<';
+
+		if ( strpos( $base, $attrib_prefix ) !== false ) {
+			$attrib_names = array();
+
+			preg_match( '/\(\?P<[^>]+>.*\)/', $base, $attrib_names, PREG_OFFSET_CAPTURE );
+
+			foreach ( $attrib_names as $attrib_name_match ) {
+				$beginning_offset = strlen( $attrib_prefix );
+				$attrib_name_end  = strpos( $attrib_name_match[0], '>', $attrib_name_match[1] );
+				$attrib_name      = substr( $attrib_name_match[0], $beginning_offset, $attrib_name_end - $beginning_offset );
+
+				if ( isset( $request[ $attrib_name ] ) ) {
+					$base = str_replace( "(?P<$attrib_name>[\d]+)", $request[ $attrib_name ], $base );
+				}
+			}
+		}
+
+		$base = add_query_arg( $request->get_query_params(), rest_url( sprintf( '/%s/%s', $this->namespace, $base ) ) );
+
+		if ( $page > 1 ) {
+			$prev_page = $page - 1;
+
+			if ( $prev_page > $max_pages ) {
+				$prev_page = $max_pages;
+			}
+
+			$prev_link = add_query_arg( 'page', $prev_page, $base );
+			$response->add_links( array(
+				'prev' => array( 'href' => $prev_link )
+			) );
+			$response->link_header( 'prev', $prev_link );
+		}
+
+		if ( $max_pages > $page ) {
+			$next_page = $page + 1;
+			$next_link = add_query_arg( 'page', $next_page, $base );
+			$response->add_links( array(
+				'next' => array( 'href' => $next_link )
+			) );
+			$response->link_header( 'next', $next_link );
+		}
+
+		return $response;
+	} // END get_items()
 
 	/**
 	 * Prepare a single product output for response.
