73f1d0525089312840d751ec9823df15ce3d5a29
seb86 <mailme@sebastiendumont.com>
2022-04-18 22:17:42 +0200

    Updated uninstall.php

--- plugins/cocart/uninstall.php
+++ plugins/cocart/uninstall.php
@@ -7,7 +7,7 @@
  * @author  Sébastien Dumont
  * @package CoCart\Uninstaller
  * @since   2.1.0
- * @version 3.2.0
+ * @version 4.0.0
  * @license GPL-2.0+
  */
 
@@ -29,7 +29,7 @@ wp_schedule_event( time() + ( 6 * HOUR_IN_SECONDS ), 'twicedaily', 'woocommerce_
 if ( defined( 'COCART_REMOVE_ALL_DATA' ) && true === COCART_REMOVE_ALL_DATA ) {
 	// Drop Tables.
 	require_once dirname( __FILE__ ) . '/includes/class-cocart-install.php';
-	CoCart_Install::drop_tables();
+	CoCart\Install::drop_tables();
 
 	// Delete options.
 	$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'cocart\_%';" );
@@ -45,13 +45,13 @@ if ( defined( 'COCART_REMOVE_ALL_DATA' ) && true === COCART_REMOVE_ALL_DATA ) {
 	require_once dirname( __FILE__ ) . '/includes/class-cocart-helpers.php';
 
 	// Delete WooCommerce Admin Notes.
-	if ( CoCart_Helpers::is_wc_admin_enabled() ) {
-		Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-activate-pro' );
-		Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-do-with-products' );
-		Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-help-improve' );
-		Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-need-help' );
-		Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-thanks-install' );
-		Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-upgrade-pro' );
+	if ( CoCart\Helpers::is_wc_admin_enabled() ) {
+		\Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-activate-pro' );
+		\Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-do-with-products' );
+		\Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-help-improve' );
+		\Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-need-help' );
+		\Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-thanks-install' );
+		\Automattic\WooCommerce\Admin\Notes\Notes::delete_notes_with_name( 'cocart-wc-admin-upgrade-pro' );
 	}
 
 	// Clear any cached data that has been removed.
