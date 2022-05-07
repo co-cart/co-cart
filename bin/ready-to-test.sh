4366755de2f24c1f8f70b3c74634f361944d59a5
seb86 <mailme@sebastiendumont.com>
2022-05-07 20:31:11 +0200

    Added ready to test build script

---
+++ bin/ready-to-test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Output colorized strings
+#
+# Color codes:
+# 0 - black
+# 1 - red
+# 2 - green
+# 3 - yellow
+# 4 - blue
+# 5 - magenta
+# 6 - cian
+# 7 - white
+output() {
+	echo "$(tput setaf "$1")$2$(tput sgr0)"
+}
+
+if [ ! -d "plugins/cocart/packages/" ]; then
+	output 1 "./plugins/cocart/packages doesn't exist!"
+	output 1 "run \"composer install\" before proceed."
+fi
+
+# Remove previous CoCart build
+if [ -d "../cocart" ]; then
+	output 1 "Removing previous CoCart build..."
+	rm -Rf "../cocart";
+	output 2 "Previous CoCart build removed."
+fi
+
+output 3 "Copying CoCart build to wp-content/plugins/..."
+cp -r ./plugins/cocart ./../cocart
+output 2 "CoCart build copied."
+
+output 3 "Cleaning remaining dev files..."
+find ./../cocart -name ".git" -type d -exec rm -rf {} +
+find ./../cocart -name "README.md" -type f -delete
+output 2 "CoCart can now be activated from your WordPress dashboard."
