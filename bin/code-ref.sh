99dfe91622e32e532fdd5cc948f630eb7ed50582
seb86 <mailme@sebastiendumont.com>
2022-05-07 23:38:52 +0200

    Added Code Reference build script

---
+++ bin/code-ref.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Output colorized strings
+#
+# Color codes:
+# 0 - black
+# 1 - red
+# 2 - green
+# 3 - yellow
+# 4 - blue
+# 5 - magenta
+# 6 - cian
+# 7 - white
+output() {
+	echo "$(tput setaf "$1")$2$(tput sgr0)"
+}
+
+if [ ! -d "plugins/cocart/packages/" ]; then
+	output 1 "./plugins/cocart/packages doesn't exist!"
+	output 1 "run \"composer install\" before proceed."
+fi
+
+# Remove previous CoCart ref build
+if [ -d "plugins/cocart-ref" ]; then
+	output 1 "Removing previous CoCart ref build..."
+	rm -Rf "plugins/cocart-ref";
+	output 2 "Previous CoCart ref build removed."
+fi
+
+output 3 "Copying CoCart build to cocart-ref..."
+cp -r ./plugins/cocart ./plugins/cocart-ref
+output 2 "CoCart build copied."
+
+output 3 "Preparing ref build..."
+find ./plugins/cocart-ref/packages -name "admin" -type d -exec rm -rf {} +
+find ./plugins/cocart-ref/packages -name "compatibility" -type d -exec rm -rf {} +
+find ./plugins/cocart-ref/packages -name "session-api" -type d -exec rm -rf {} +
+find ./plugins/cocart-ref/packages -name "third-party" -type d -exec rm -rf {} +
+find ./plugins/cocart-ref -name ".git" -type d -exec rm -rf {} +
+find ./plugins/cocart-ref -name "README.md" -type f -delete
+find ./plugins/cocart-ref -name "src" -type d -exec rm -rf {} +
+
+output 2 "Code Reference can now import this build."
