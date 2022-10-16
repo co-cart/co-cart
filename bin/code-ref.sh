0fddfdb9b2f4d84ead881b5334f1c6fe440708d3
Sébastien Dumont <mailme@sebastiendumont.com>
2022-10-16 12:01:53 +0200

    Code ref build script updated

--- bin/code-ref.sh
+++ bin/code-ref.sh
@@ -43,9 +43,12 @@ find ./plugins/cocart-ref -name "languages" -type d -exec rm -rf {} +
 find ./plugins/cocart-ref -name "vendors" -type d -exec rm -rf {} +
 find ./plugins/cocart-ref -name "vendor" -type d -exec rm -rf {} +
 find ./plugins/cocart-ref -name ".git" -type d -exec rm -rf {} +
+find ./plugins/cocart-ref -name ".gitignore" -type f -exec rm -rf {} +
 find ./plugins/cocart-ref -name "README.md" -type f -delete
 find ./plugins/cocart-ref -name "src" -type d -exec rm -rf {} +
 find ./plugins/cocart-ref -name "composer.json" -type f -delete
+find ./plugins/cocart-ref -name "license.txt" -type f -delete
+find ./plugins/cocart-ref -name "readme.txt" -type f -delete
 find ./plugins/cocart-ref -name "load-package.php" -type f -delete
 find ./plugins/cocart-ref -name "cart-rest-api-for-woocommerce.php" -type f -delete
 find ./plugins/cocart-ref -name "uninstall.php" -type f -delete
@@ -66,23 +69,39 @@ cp -r ./plugins/cocart-ref ./plugins/cocart-ref-source
 output 3 "Code reference build cloned!"
 
 output 6 "Replacing strings for import compatibility..."
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/Abstracts\\CoCart_Cart_Extension_Callback/Abstracts_CoCart_Cart_Extension_Callback/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Abstracts\\Session/CoCart_Abstracts_Session/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Abstracts/CoCart_Abstracts/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\CoCart_Data_Exception/CoCart_Data_Exception/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Core/CoCart_Core/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Help/CoCart_Help/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Install/CoCart_Install/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Logger/CoCart_Logger/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Session\\Handler/CoCart_Session_Handler/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Session/CoCart_Session/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Status/CoCart_Status/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\ProductsAPI\\DateTime/CoCart_ProductsAPI_DateTime/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\RestApi\\Authentication/CoCart_RestApi_Authentication/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\RestApi\\CartCache/CoCart_RestApi_CartCache/g' {} \;
-find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\RestApi\\Callbacks/CoCart_RestApi_Callbacks/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\RestApi\\Callbacks\\UpdateCart/CoCart_RestApi_Callbacks_UpdateCart/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\RestApi\\Callbacks/CoCart_RestApi_Callbacks/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\RestApi/CoCart_RestApi/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Utilities\\RateLimits/CoCart_Utilities_RateLimits/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/CoCart\\Utilities/CoCart_Utilities/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\Exception/Exception/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\PasswordHash/PasswordHash/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WP_REST_Response/WP_REST_Response/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WP_Error/WP_Error/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WC_Background_Process/WC_Background_Process/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WC_Cart/WC_Cart/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WC_Cache_Helper/WC_Cache_Helper/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WC_Customer/WC_Customer/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WC_Data_Store/WC_Data_Store/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WC_Rate_Limiter/WC_Rate_Limiter/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WC_Session/WC_Session/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\WC_Validation/WC_Validation/g' {} \;
 find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\Automattic\\WooCommerce\\Checkout\\Helpers\\ReserveStock/Automattic_WooCommerce_Checkout_Helpers_ReserveStock/g' {} \;
+find ./plugins/cocart-ref/includes/ -name '*.php' -type f -exec sed -i -e 's/\\Automattic\\WooCommerce\\Utilities\\StringUtil/Automattic_WooCommerce_Utilities_StringUtil/g' {} \;
 output 3 "Strings replaced."
 
 output 7 "Code reference can now be imported!"
