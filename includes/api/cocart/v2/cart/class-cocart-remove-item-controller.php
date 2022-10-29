328ff538db698dc77263fbf86be1a95914efe63c
Sébastien Dumont <mailme@sebastiendumont.com>
2022-10-29 19:03:18 +0200

    Improved getting request parameters for delete method endpoints.

--- includes/api/cocart/v2/cart/class-cocart-remove-item-controller.php
+++ includes/api/cocart/v2/cart/class-cocart-remove-item-controller.php
@@ -58,13 +58,14 @@ class CoCart_Remove_Item_v2_Controller extends CoCart_Cart_V2_Controller {
 	 *
 	 * @access  public
 	 * @since   1.0.0
-	 * @version 3.1.0
+	 * @version 3.7.8
 	 * @param   WP_REST_Request $request Full details about the request.
 	 * @return  WP_REST_Response
 	 */
 	public function remove_item( $request = array() ) {
 		try {
-			$item_key = ! isset( $request['item_key'] ) ? '0' : sanitize_text_field( wp_unslash( wc_clean( $request['item_key'] ) ) );
+			$request_params = $request->get_params();
+			$item_key       = ! isset( $request_params['item_key'] ) ? '0' : wc_clean( wp_unslash( sanitize_text_field( $request_params['item_key'] ) ) );
 
 			$item_key = $this->throw_missing_item_key( $item_key, 'remove' );
 
