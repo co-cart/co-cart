b9e44b97f0717d40d17d2b8f996ce4358e924ab6
Sébastien Dumont <mailme@sebastiendumont.com>
2023-06-04 00:12:01 +0200

    Moved cart taxes to it's own function

--- plugins/cocart/includes/classes/rest-api/controllers/v2/cart/class-cocart-rest-cart-v2-controller.php
+++ plugins/cocart/includes/classes/rest-api/controllers/v2/cart/class-cocart-rest-cart-v2-controller.php
@@ -337,27 +337,7 @@ class CoCart_REST_Cart_v2_Controller extends CoCart_API_Controller {
 		}
 
 		if ( rest_is_field_included( 'taxes', $fields ) ) {
-			$cart['taxes'] = array();
-
-			// Return calculated tax based on store settings and customer details.
-			if ( wc_tax_enabled() && ! $this->get_cart_instance()->display_prices_including_tax() ) {
-				$taxable_address = WC()->customer->get_taxable_address();
-				$estimated_text  = '';
-
-				if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
-					/* translators: %s location. */
-					$estimated_text = sprintf( ' ' . esc_html__( '(estimated for %s)', 'cart-rest-api-for-woocommerce' ), WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
-				}
-
-				if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
-					$cart['taxes'] = $this->get_tax_lines( $this->get_cart_instance() );
-				} else {
-					$cart['taxes'] = array(
-						'label' => esc_html( WC()->countries->tax_or_vat() ) . $estimated_text,
-						'total' => apply_filters( 'cocart_cart_totals_taxes_total', $this->get_cart_instance()->get_taxes_total(), $request ),
-					);
-				}
-			}
+			$cart['taxes'] = $this->get_taxes( $request );
 		}
 
 		if ( rest_is_field_included( 'totals', $fields ) ) {
@@ -931,6 +911,41 @@ class CoCart_REST_Cart_v2_Controller extends CoCart_API_Controller {
 			return CoCart_Response::get_error_response( $e->getErrorCode(), $e->getMessage(), $e->getCode(), $e->getAdditionalData() );
 		}
 	} // END get_cart_key()
+
+	/**
+	 * Get cart taxes.
+	 *
+	 * @access protected
+	 *
+	 * @since 4.0.0 Introduced.
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 *
+	 * @return array
+	 */
+	protected function get_taxes( $request = array() ) {
+		// Return calculated tax based on store settings and customer details.
+		if ( wc_tax_enabled() && ! $this->get_cart_instance()->display_prices_including_tax() ) {
+			$taxable_address = WC()->customer->get_taxable_address();
+			$estimated_text  = '';
+
+			if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
+				/* translators: %s location. */
+				$estimated_text = sprintf( ' ' . esc_html__( '(estimated for %s)', 'cart-rest-api-for-woocommerce' ), WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
+			}
+
+			if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
+				$taxes = $this->get_tax_lines( $request );
+			} else {
+				$taxes = array(
+					'label' => esc_html( WC()->countries->tax_or_vat() ) . $estimated_text,
+					'total' => apply_filters( 'cocart_cart_totals_taxes_total', $this->get_cart_instance()->get_taxes_total(), $request ),
+				);
+			}
+		}
+
+		return $taxes;
+	} // END get_taxes()
 
 	/**
 	 * Get tax lines from the cart and format to match schema.
