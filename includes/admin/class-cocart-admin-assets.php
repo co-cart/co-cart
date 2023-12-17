f76b019d8ce630cb8f7cb2a500506092bdffea60
Sébastien Dumont <mailme@sebastiendumont.com>
2023-12-17 20:06:29 +0100

    Improved enqueue of admin stylesheets

--- includes/admin/class-cocart-admin-assets.php
+++ includes/admin/class-cocart-admin-assets.php
@@ -34,16 +34,17 @@ if ( ! class_exists( 'CoCart_Admin_Assets' ) ) {
 		/**
 		 * Registers and enqueue Stylesheets.
 		 *
-		 * @access  public
-		 * @since   1.2.0
-		 * @version 3.0.17
+		 * @access public
+		 *
+		 * @since 1.2.0 Introduced.
+		 * @since 3.10.0 Use of $hook_suffix parameter was added instead of using the current screen.
+		 *
+		 * @param string $hook_suffix The current admin page.
 		 */
-		public function admin_styles() {
-			$screen    = get_current_screen();
-			$screen_id = $screen ? $screen->id : '';
-			$suffix    = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+		public function admin_styles( $hook_suffix ) {
+			$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
-			if ( in_array( $screen_id, CoCart_Helpers::cocart_get_admin_screens() ) ) {
+			if ( strpos( $hook_suffix, 'cocart' ) !== false || ( isset( $_GET['page'] ) && strpos( trim( sanitize_key( wp_unslash( $_GET['page'] ) ) ), 'cocart' ) === 0 ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 				wp_register_style( COCART_SLUG . '_admin', COCART_URL_PATH . '/assets/css/admin/cocart' . $suffix . '.css', array(), COCART_VERSION );
 				wp_enqueue_style( COCART_SLUG . '_admin' );
 				wp_style_add_data( COCART_SLUG . '_admin', 'rtl', 'replace' );
@@ -59,11 +60,14 @@ if ( ! class_exists( 'CoCart_Admin_Assets' ) ) {
 		/**
 		 * Adds admin body class for CoCart page.
 		 *
-		 * @access  public
-		 * @since   1.2.0
+		 * @access public
+		 *
+		 * @since   1.2.0 Introduced.
 		 * @version 3.0.7
-		 * @param   string $classes - Classes already registered.
-		 * @return  string $classes - All classes registered.
+		 *
+		 * @param string $classes Classes already registered.
+		 *
+		 * @return string $classes All classes registered.
 		 */
 		public function admin_body_class( $classes ) {
 			$screen    = get_current_screen();
