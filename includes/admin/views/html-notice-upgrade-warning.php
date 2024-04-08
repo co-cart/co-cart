98438442478e6c9aa3dcdb50a8d4579cdd986c90
Sébastien Dumont <mailme@sebastiendumont.com>
2024-04-08 07:59:17 +0200

    Updated upgrade warning notice

--- includes/admin/views/html-notice-upgrade-warning.php
+++ includes/admin/views/html-notice-upgrade-warning.php
@@ -5,7 +5,7 @@
  * @author   Sébastien Dumont
  * @package  CoCart\Admin\Views
  * @since    1.2.3
- * @version  3.11.3
+ * @version  3.13.0
  * @license  GPL-2.0+
  */
 
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<?php
 			printf(
 				/* translators: %1$s: CoCart, %2$s: CoCart's Next Version */
-				esc_html__( 'Version %2$s of %1$s is in the works and is going to be better than ever with many improvements and options including batch support, rate limiter, and improved security. A newly added settings page will allow you to quickly configure %1$s as you need and many more action hooks and filters for developers. It is currently in the beta testing stages.', 'cart-rest-api-for-woocommerce' ),
+				esc_html__( 'Version %2$s of %1$s is in the works and is going to be better than ever with many improvements and options including improved security. A newly added settings page will allow you to quickly configure %1$s as you need and many more action hooks and filters for developers. It is currently in development.', 'cart-rest-api-for-woocommerce' ),
 				'CoCart',
 				esc_attr( COCART_NEXT_VERSION )
 			);
