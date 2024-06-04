04a259851880724336bec77146071893550b957b
Sébastien Dumont <mailme@sebastiendumont.com>
2024-06-04 23:36:55 +0200

    If a field has not provided a value then unset it.

--- includes/callbacks/update-customer.php
+++ includes/callbacks/update-customer.php
@@ -95,6 +95,11 @@ class CoCart_Update_Customer_Callback extends CoCart_Cart_Extension_Callback {
 				// Prepares customer billing field.
 				in_array( $key, $params ) && ! empty( $params[ $key ] ) ? $details[ 'billing_' . $key ] = wc_clean( wp_unslash( $params[ $key ] ) ) : ''; // phpcs:ignore WordPress.PHP.StrictInArray.MissingTrueStrict
 
+				// If a field has not provided a value then unset it.
+				if ( empty( $details[ $key ] ) ) {
+					unset( $details[ $key ] );
+				}
+
 				// Validates customer billing fields for email, phone, country and postcode.
 				if ( 'email' === $key && ! empty( $details['billing_email'] ) ) {
 					if ( ! \WC_Validation::is_email( $details['billing_email'] ) ) {
