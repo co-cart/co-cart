ba1af30df2a2e14abe4cf7fa1d4cf7b363d64c78
Sébastien Dumont <mailme@sebastiendumont.com>
2024-07-15 17:07:36 +0200

    Updated plugin header

--- cart-rest-api-for-woocommerce.php
+++ cart-rest-api-for-woocommerce.php
@@ -9,8 +9,15 @@
  * Text Domain: cart-rest-api-for-woocommerce
  * Domain Path: /languages/
  * Requires at least: 5.6
+ * Tested up to: 6.6
  * Requires PHP: 7.4
  * Requires Plugins: woocommerce
+ * WC requires at least: 7.0
+ * WC tested up to: 9.1
+ *
+ * Copyright:   CoCart Headless, LLC
+ * License:     GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
  * @package CoCart
  */
