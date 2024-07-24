0c4c109792715e9a21007d552878f2b8ee733f3b
Sébastien Dumont <mailme@sebastiendumont.com>
2024-07-24 18:21:05 +0200

    Load cart from session 2.0 ready for testing

--- includes/classes/class-cocart-session.php
+++ includes/classes/class-cocart-session.php
@@ -5,7 +5,7 @@
  * @author  Sébastien Dumont
  * @package CoCart\Classes
  * @since   2.1.0 Introduced.
- * @version 4.2.0
+ * @version 4.4.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -29,82 +29,18 @@ class CoCart_Load_Cart {
 	 * @ignore Function ignored when parsed into Code Reference.
 	 */
 	public function __construct() {
-		// Loads a cart in session if still valid.
-		add_action( 'woocommerce_load_cart_from_session', array( $this, 'load_cart_action' ), 10 );
+		// Loads a users cart.
+		add_action( 'wp', array( $this, 'maybe_load_users_cart' ), 0 );
+
+		// Loads a cart in session if valid.
+		add_action( 'woocommerce_load_cart_from_session', array( $this, 'load_cart_action' ) );
 
 		// Append cart to load for proceed to checkout url.
 		add_action( 'woocommerce_get_checkout_url', array( $this, 'proceed_to_checkout' ) );
 	} // END __construct()
 
 	/**
-	 * Returns true or false if the cart key is saved in the database.
-	 *
-	 * @todo Deprecate in the future.
-	 *
-	 * @access public
-	 *
-	 * @since 2.1.0 Introduced.
-	 *
-	 * @uses WC()->session->get_session()
-	 *
-	 * @param string $cart_key Requested cart key.
-	 *
-	 * @return boolean
-	 */
-	public function is_cart_saved( $cart_key ) {
-		$cart_saved = WC()->session->get_session( $cart_key );
-
-		if ( ! empty( $cart_saved ) ) {
-			return true;
-		}
-
-		return false;
-	} // END is_cart_saved()
-
-	/**
-	 * Clears all carts from the database.
-	 *
-	 * @access public
-	 *
-	 * @static
-	 *
-	 * @since 2.1.0 Introduced
-	 *
-	 * @deprecated 3.1.2 Replaced with `cocart_task_clear_carts()` instead.
-	 *
-	 * @see cocart_task_clear_carts()
-	 */
-	public static function clear_carts() {
-		cocart_deprecated_function( 'clear_carts', '3.1.2', 'cocart_task_clear_carts' );
-
-		cocart_task_clear_carts();
-	} // END clear_cart()
-
-	/**
-	 * Cleans up carts from the database that have expired.
-	 *
-	 * @access public
-	 *
-	 * @static
-	 *
-	 * @since 2.1.0 Introduced
-	 *
-	 * @deprecated 3.1.2 Replaced with `cocart_task_cleanup_carts()` instead.
-	 *
-	 * @see cocart_task_cleanup_carts()
-	 */
-	public static function cleanup_carts() {
-		cocart_deprecated_function( 'cleanup_carts', '3.1.2', 'cocart_task_cleanup_carts' );
-
-		cocart_task_cleanup_carts();
-	} // END cleanup_carts()
-
-	/**
-	 * Load cart action.
-	 *
-	 * Loads a cart in session if still valid and overrides the current cart.
-	 * Unless specified not to override, the carts will merge the current cart
-	 * and the loaded cart items together.
+	 * Loads a cart in session if valid.
 	 *
 	 * @access public
 	 *
@@ -112,228 +48,80 @@ class CoCart_Load_Cart {
 	 *
 	 * @since 2.1.0 Introduced.
 	 * @since 4.2.0 Replaced `wc_nocache_headers()` with `cocart_nocache_headers()`.
+	 * @since 4.4.0 No longer return debug logs, merge carts together or optionally notify customers of any messages.
 	 *
 	 * @uses CoCart_Load_Cart::maybe_load_cart()
 	 * @uses CoCart_Load_Cart::get_action_query()
-	 * @uses CoCart_Logger::log()
-	 * @uses get_user_by()
 	 * @uses is_user_logged_in()
-	 * @uses wp_get_current_user()
+	 * @uses wc_clear_notices()
 	 * @uses wc_add_notice()
 	 *
 	 * @see cocart_nocache_headers()
 	 */
 	public static function load_cart_action() {
 		if ( self::maybe_load_cart() ) {
-			$action          = self::get_action_query();
-			$cart_key        = isset( $_REQUEST[ $action ] ) ? trim( sanitize_text_field( wp_unslash( $_REQUEST[ $action ] ) ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-			$override_cart   = true;  // Override the cart by default.
-			$notify_customer = false; // Don't notify the customer by default.
+			$action         = self::get_action_query();
+			$cart_key       = isset( $_REQUEST[ $action ] ) ? sanitize_text_field( wp_unslash( $_REQUEST[ $action ] ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			$is_cart_loaded = true;
 
 			cocart_nocache_headers();
 
-			// Check the cart doesn't belong to a registered user - only guest carts should be loadable from session.
-			$user = get_user_by( 'id', $cart_key );
+			// Get the requested cart in the database.
+			$requested_cart = WC()->session->get_session( $cart_key );
 
-			// If the user exists, the cart key is for a registered user so we should just return.
-			if ( ! empty( $user ) ) {
-				if ( is_user_logged_in() ) {
-					$current_user = wp_get_current_user();
-					$user_id      = $current_user->ID;
+			if ( empty( $requested_cart ) ) {
+				$is_cart_loaded = false;
 
-					// Compare the user ID with the cart key.
-					if ( $user_id === $cart_key ) {
-						CoCart_Logger::log(
-							sprintf(
-								/* translators: %s: cart key */
-								__( 'Cart key "%s" is already loaded as the currently logged in user.', 'cart-rest-api-for-woocommerce' ),
-								$cart_key
-							),
-							'error'
-						);
-					} else {
-						CoCart_Logger::log(
-							sprintf(
-								/* translators: %s: cart key */
-								__( 'Customer is logged in as a different user. Cart key "%s" cannot be loaded into session for a different user.', 'cart-rest-api-for-woocommerce' ),
-								$cart_key
-							),
-							'error'
-						);
-					}
-				} else {
-					CoCart_Logger::log(
-						__( 'Cart key is recognized as a registered user on site. Cannot be loaded into session as a guest.', 'cart-rest-api-for-woocommerce' ),
-						'error'
-					);
-				}
-
-				// Display notice to developer if debug mode is enabled.
-				if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-					wc_add_notice(
-						__( 'Technical error made! See error log for reason.', 'cart-rest-api-for-woocommerce' ),
-						'error'
-					);
-				}
-
-				return;
-			}
-
-			// At this point, the cart should load into session with no issues as we have passed verification.
-
-			// Check if we are keeping the cart currently set via the web.
-			if ( ! empty( $_REQUEST['keep-cart'] ) && is_bool( $_REQUEST['keep-cart'] ) !== true ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-				$override_cart = false;
-			}
-
-			// Check if we are notifying the customer via the web.
-			if ( ! empty( $_REQUEST['notify'] ) && is_bool( $_REQUEST['notify'] ) !== true ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-				$notify_customer = true;
-			}
-
-			// Get the cart in the database.
-			$stored_cart = WC()->session->get_session( $cart_key );
-
-			if ( empty( $stored_cart ) ) {
-				CoCart_Logger::log(
-					sprintf(
-						/* translators: %s: cart key */
-						__( 'Unable to find cart for: %s', 'cart-rest-api-for-woocommerce' ),
-						$cart_key
-					),
-					'info'
-				);
-
-				if ( $notify_customer ) {
-					wc_add_notice(
-						__( 'Sorry but this cart has expired!', 'cart-rest-api-for-woocommerce' ),
-						'error'
-					);
-				}
-
-				return;
-			}
-
-			// Get the cart currently in session if any.
-			$cart_in_session = WC()->session->get( 'cart', array() );
-
-			$new_cart = array();
-
-			$new_cart['cart']                       = maybe_unserialize( $stored_cart['cart'] );
-			$new_cart['applied_coupons']            = maybe_unserialize( $stored_cart['applied_coupons'] );
-			$new_cart['coupon_discount_totals']     = maybe_unserialize( $stored_cart['coupon_discount_totals'] );
-			$new_cart['coupon_discount_tax_totals'] = maybe_unserialize( $stored_cart['coupon_discount_tax_totals'] );
-			$new_cart['removed_cart_contents']      = maybe_unserialize( $stored_cart['removed_cart_contents'] );
-
-			if ( ! empty( $stored_cart['chosen_shipping_methods'] ) ) {
-				$new_cart['chosen_shipping_methods'] = maybe_unserialize( $stored_cart['chosen_shipping_methods'] );
-			}
-
-			if ( ! empty( $stored_cart['cart_fees'] ) ) {
-				$new_cart['cart_fees'] = maybe_unserialize( $stored_cart['cart_fees'] );
-			}
-
-			// Checks for any items cached.
-			if ( ! empty( $stored_cart['cart_cached'] ) ) {
-				$new_cart['cart_cached'] = maybe_unserialize( $stored_cart['cart_cached'] );
-			}
-
-			// Check if we are overriding the cart currently in session via the web.
-			if ( $override_cart ) {
-				// Only clear the cart if it's not already empty.
-				if ( ! WC()->cart->is_empty() ) {
-					WC()->cart->empty_cart( false );
-
-					/**
-					 * Hook: cocart_load_cart_override.
-					 *
-					 * Manipulate the overriding cart before it set in session.
-					 *
-					 * @since 2.1.0 Introduced.
-					 */
-					do_action( 'cocart_load_cart_override', $new_cart, $stored_cart );
-				}
-			} else {
-				$new_cart_content = array_merge( $new_cart['cart'], maybe_unserialize( $cart_in_session ) );
-				/**
-				 * Filter allows you to adjust the merged cart contents.
-				 *
-				 * @since 2.1.0 Introduced.
-				 */
-				$new_cart['cart']                       = apply_filters( 'cocart_merge_cart_content', $new_cart_content, $new_cart['cart'], $cart_in_session );
-				$new_cart['applied_coupons']            = array_unique( array_merge( $new_cart['applied_coupons'], WC()->cart->get_applied_coupons() ) );
-				$new_cart['coupon_discount_totals']     = array_merge( $new_cart['coupon_discount_totals'], WC()->cart->get_coupon_discount_totals() );
-				$new_cart['coupon_discount_tax_totals'] = array_merge( $new_cart['coupon_discount_tax_totals'], WC()->cart->get_coupon_discount_tax_totals() );
-				$new_cart['removed_cart_contents']      = array_merge( $new_cart['removed_cart_contents'], WC()->cart->get_removed_cart_contents() );
-
-				/**
-				 * Hook: cocart_load_cart.
-				 *
-				 * Manipulate the merged cart before it set in session.
-				 *
-				 * @since 2.1.0 Introduced.
-				 */
-				do_action( 'cocart_load_cart', $new_cart, $stored_cart, $cart_in_session );
-			}
-
-			// Destroy cart if user is a guest customer before creating a new one.
-			if ( ! is_user_logged_in() && self::maybe_use_cookie_monster() ) {
-				WC()->session->delete_cart( WC()->session->get_customer_id() );
-			}
-
-			// Sets the php session data for the loaded cart.
-			WC()->session->set( 'cart', $new_cart['cart'] );
-			WC()->session->set( 'applied_coupons', $new_cart['applied_coupons'] );
-			WC()->session->set( 'coupon_discount_totals', $new_cart['coupon_discount_totals'] );
-			WC()->session->set( 'coupon_discount_tax_totals', $new_cart['coupon_discount_tax_totals'] );
-			WC()->session->set( 'removed_cart_contents', $new_cart['removed_cart_contents'] );
-
-			if ( ! empty( $new_cart['chosen_shipping_methods'] ) ) {
-				WC()->session->set( 'chosen_shipping_methods', $new_cart['chosen_shipping_methods'] );
-			}
-
-			if ( ! empty( $new_cart['cart_fees'] ) ) {
-				WC()->session->set( 'cart_fees', $new_cart['cart_fees'] );
-			}
-
-			if ( ! empty( $new_cart['cart_cached'] ) ) {
-				WC()->session->set( 'cart_cached', $new_cart['cart_cached'] );
-			}
-
-			// Set loaded cart for guest customer.
-			if ( ! is_user_logged_in() && self::maybe_use_cookie_monster() ) {
-				WC()->session->set_cart_hash();
-				WC()->session->set_customer_id( $cart_key );
-				WC()->session->set_cart_expiration();
-				WC()->session->set_customer_session_cookie( true );
-			}
-
-			// If true, notify the customer that there cart has transferred over via the web.
-			if ( ! empty( $new_cart ) && $notify_customer ) {
+				wc_clear_notices();
 				wc_add_notice(
-					apply_filters( 'cocart_cart_loaded_successful_message',
-						sprintf(
-							/* translators: %1$s: Start of link to Shop archive. %2$s: Start of link to checkout page. %3$s: Closing link tag. */
-							__( 'Your 🛒 cart has been transferred over. You may %1$scontinue shopping%3$s or %2$scheckout%3$s.', 'cart-rest-api-for-woocommerce' ),
-							'<a href="' . wc_get_page_permalink( 'shop' ) . '">',
-							'<a href="' . wc_get_checkout_url() . '">',
-							'</a>'
-						)
-					),
-					'notice'
+					esc_html__( 'Cart is not valid! If this is an error, contact for help.', 'cart-rest-api-for-woocommerce' ),
+					'error'
 				);
 			}
 
-			/**
-			 * Hook: cocart_cart_loaded.
-			 *
-			 * Fires once a cart has loaded. Can be used to trigger a webhook.
-			 *
-			 * @since 3.8.0 Introduced.
-			 *
-			 * @param string $cart_key The cart key.
-			 */
-			do_action( 'cocart_cart_loaded', $cart_key );
+			if ( $is_cart_loaded ) {
+				// Destroy guest cart if one already existed.
+				if ( ! is_user_logged_in() && WC()->session->get_customer_id() !== $cart_key ) {
+					WC()->session->delete_cart( WC()->session->get_customer_id() );
+				}
+
+				// Sets the php session data for the loaded cart.
+				WC()->session->set( 'cart', maybe_unserialize( $requested_cart['cart'] ) );
+				WC()->session->set( 'applied_coupons', maybe_unserialize( $requested_cart['applied_coupons'] ) );
+				WC()->session->set( 'coupon_discount_totals', maybe_unserialize( $requested_cart['coupon_discount_totals'] ) );
+				WC()->session->set( 'coupon_discount_tax_totals', maybe_unserialize( $requested_cart['coupon_discount_tax_totals'] ) );
+				WC()->session->set( 'removed_cart_contents', maybe_unserialize( $requested_cart['removed_cart_contents'] ) );
+
+				if ( ! empty( $requested_cart['chosen_shipping_methods'] ) ) {
+					WC()->session->set( 'chosen_shipping_methods', maybe_unserialize( $requested_cart['chosen_shipping_methods'] ) );
+				}
+
+				if ( ! empty( $requested_cart['cart_fees'] ) ) {
+					WC()->session->set( 'cart_fees', maybe_unserialize( $requested_cart['cart_fees'] ) );
+				}
+
+				// Checks for any items cached.
+				if ( ! empty( $requested_cart['cart_cached'] ) ) {
+					WC()->session->set( 'cart_cached', maybe_unserialize( $requested_cart['cart_cached'] ) );
+				}
+
+				// Setup cart session.
+				WC()->session->set_customer_id( $cart_key );
+				WC()->session->set_cart_hash();
+				WC()->session->set_session_expiration();
+				WC()->session->set_customer_session_cookie( true );
+
+				/**
+				 * Hook: cocart_cart_loaded.
+				 *
+				 * Fires once a cart has loaded. Can be used to trigger a webhook.
+				 *
+				 * @since 3.8.0 Introduced.
+				 *
+				 * @param string $cart_key The cart key.
+				 */
+				do_action( 'cocart_cart_loaded', $cart_key );
+			}
 		}
 	} // END load_cart_action()
 
@@ -438,6 +226,101 @@ class CoCart_Load_Cart {
 	protected static function maybe_use_cookie_monster() {
 		return apply_filters( 'cocart_use_cookie_monster', true );
 	} // END maybe_use_cookie_monster()
+
+	/**
+	 * Loads a users cart.
+	 *
+	 * If the cart is associated with a registered user then we make sure that
+	 * the user is logged in to help with managing the cart session.
+	 *
+	 * @access public
+	 *
+	 * @static
+	 *
+	 * @since 4.4.0 Introduced.
+	 *
+	 * @uses CoCart_Load_Cart::maybe_load_cart()
+	 * @uses CoCart_Load_Cart::get_action_query()
+	 * @uses is_user_logged_in()
+	 * @uses get_user_by()
+	 * @uses wp_get_current_user()
+	 * @uses wp_logout()
+	 * @uses wp_set_auth_cookie()
+	 * @uses wp_safe_redirect()
+	 * @uses wc_get_checkout_url()
+	 */
+	public static function maybe_load_users_cart() {
+		if ( self::maybe_load_cart() ) {
+			$action   = self::get_action_query();
+			$cart_key = isset( $_REQUEST[ $action ] ) ? sanitize_text_field( wp_unslash( $_REQUEST[ $action ] ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			$hash     = isset( $_REQUEST['c_hash'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['c_hash'] ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+
+			/**
+			 * Filter allows you to change where to redirect should loading the cart fail.
+			 *
+			 * @since 4.4.0 Introduced.
+			 */
+			$redirect_home = apply_filters( 'cocart_load_cart_redirect_home', home_url() );
+
+			if ( ! empty( $cart_key ) ) {
+				// Return if the cart key is not all digits.
+				if ( ! ctype_digit( $cart_key ) ) {
+					return;
+				}
+
+				$customer_id = absint( $cart_key );
+			}
+
+			// Get cart hash to check if it matches later.
+			$cart_hash = cocart_get_cart_hash( $customer_id );
+
+			// No cart hash or found then just redirect home. It's possible the session does not exist.
+			if ( empty( $hash ) || $cart_hash !== $hash ) {
+				// Determine if we redirect or not based on the cart key value type.
+				if ( is_int( $customer_id ) && $cart_key == $customer_id ) {
+					wp_safe_redirect( $redirect_home );
+					exit;
+				} else {
+					return;
+				}
+			}
+
+			// Check if the cart belongs to a registered user.
+			$user = get_user_by( 'id', $customer_id );
+
+			// If the user exists and the cart hash match, then the customers ID is a registered user so login user and load the cart.
+			if ( ! empty( $user ) && $cart_hash === $hash ) {
+				if ( is_user_logged_in() ) {
+					$current_user = wp_get_current_user();
+					$user_id      = $current_user->ID;
+
+					// Compare the user ID with the customers ID. If not the same user then logout.
+					if ( $user_id !== $customer_id ) {
+						wp_logout(); // Logout current user.
+					}
+				} else {
+					wp_set_auth_cookie( $customer_id ); // Login new user.
+				}
+
+				/**
+				 * Fires after the user has successfully logged in.
+				 *
+				 * Note: This action hook is forked from WP Core so we can trigger any WooCommerce related hooks.
+				 *
+				 * @ignore Hook ignored when parsed into Code Reference.
+				 *
+				 * @param string  $user_login Username.
+				 * @param WP_User $user       WP_User object of the logged-in user.
+				 */
+				do_action( 'wp_login', $user->user_login, $user );
+
+				wp_safe_redirect( wc_get_checkout_url() );
+				exit;
+			}
+
+			return;
+		}
+	} // END maybe_load_users_cart()
 } // END class
 
 return new CoCart_Load_Cart();
