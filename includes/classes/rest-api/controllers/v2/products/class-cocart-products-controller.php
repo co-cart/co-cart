bf8d6d6453752d46e5a54da08b08bbf5a5cf8782
Sébastien Dumont <mailme@sebastiendumont.com>
2024-07-29 20:07:21 +0200

    Functions properly deprecated

--- includes/classes/rest-api/controllers/v2/products/class-cocart-products-controller.php
+++ includes/classes/rest-api/controllers/v2/products/class-cocart-products-controller.php
@@ -387,62 +387,7 @@ class CoCart_REST_Products_V2_Controller extends CoCart_Products_Controller {
 	protected function get_images( $product ) {
 		cocart_deprecated_function( 'CoCart_REST_Products_V2_Controller::get_images', '4.2.0', 'CoCart_Utilities_Product_Helpers::get_images' );
 
-		$images           = array();
-		$attachment_ids   = array();
-		$attachment_sizes = apply_filters( 'cocart_products_image_sizes', array_merge( get_intermediate_image_sizes(), array( 'full', 'custom' ) ) );
-
-		// Add featured image.
-		if ( $product->get_image_id() ) {
-			$attachment_ids[] = $product->get_image_id();
-		}
-
-		// Add gallery images.
-		$attachment_ids = array_merge( $attachment_ids, $product->get_gallery_image_ids() );
-
-		$attachments = array();
-
-		// Build image data.
-		foreach ( $attachment_ids as $position => $attachment_id ) {
-			$attachment_post = get_post( $attachment_id );
-			if ( is_null( $attachment_post ) ) {
-				continue;
-			}
-
-			// Get each image size of the attachment.
-			foreach ( $attachment_sizes as $size ) {
-				$attachments[ $size ] = current( wp_get_attachment_image_src( $attachment_id, $size ) );
-			}
-
-			$featured = $position === 0 ? true : false; // phpcs:ignore WordPress.PHP.YodaConditions.NotYoda
-
-			$images[] = array(
-				'id'       => (int) $attachment_id,
-				'src'      => $attachments,
-				'name'     => get_the_title( $attachment_id ),
-				'alt'      => get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ),
-				'position' => (int) $position,
-				'featured' => $featured,
-			);
-		}
-
-		// Set a placeholder image if the product has no images set.
-		if ( empty( $images ) ) {
-			// Get each image size of the attachment.
-			foreach ( $attachment_sizes as $size ) {
-				$attachments[ $size ] = wc_placeholder_img_src( $size );
-			}
-
-			$images[] = array(
-				'id'       => 0,
-				'src'      => $attachments,
-				'name'     => __( 'Placeholder', 'cart-rest-api-for-woocommerce' ),
-				'alt'      => __( 'Placeholder', 'cart-rest-api-for-woocommerce' ),
-				'position' => 0,
-				'featured' => true,
-			);
-		}
-
-		return $images;
+		return CoCart_Utilities_Product_Helpers::get_images( $product );
 	} // END get_images()
 
 	/**
@@ -509,7 +454,7 @@ class CoCart_REST_Products_V2_Controller extends CoCart_Products_Controller {
 				'price'         => cocart_prepare_money_response( $price_function( $product ), wc_get_price_decimals() ),
 				'regular_price' => cocart_prepare_money_response( $price_function( $product, array( 'price' => $regular_price ) ), wc_get_price_decimals() ),
 				'sale_price'    => $product->get_sale_price( 'view' ) ? cocart_prepare_money_response( $price_function( $product, array( 'price' => $sale_price ) ), wc_get_price_decimals() ) : '',
-				'price_range'   => $this->get_price_range( $product, $tax_display_mode ),
+				'price_range'   => CoCart_Utilities_Product_Helpers::get_price_range( $product, $tax_display_mode ),
 				'on_sale'       => $product->is_on_sale( 'view' ),
 				'date_on_sale'  => array(
 					'from'     => ! is_null( $date_on_sale_from ) ? cocart_prepare_date_response( $date_on_sale_from->date( 'Y-m-d\TH:i:s' ), false ) : null,
@@ -572,7 +517,7 @@ class CoCart_REST_Products_V2_Controller extends CoCart_Products_Controller {
 				'purchase_quantity' => $purchase_quantity,
 				'rest_url'          => $this->add_to_cart_rest_url( $product, $type ),
 			),
-			'meta_data'          => $this->get_meta_data( $product ),
+			'meta_data'          => CoCart_Utilities_Product_Helpers::get_meta_data( $product ),
 		);
 
 		return $data;
@@ -985,70 +930,7 @@ class CoCart_REST_Products_V2_Controller extends CoCart_Products_Controller {
 	public function get_price_range( $product, $tax_display_mode = '' ) {
 		cocart_deprecated_function( 'CoCart_REST_Products_V2_Controller::get_price_range', '4.2.0', 'CoCart_Utilities_Product_Helpers::get_price_range' );
 
-		$tax_display_mode = $this->get_tax_display_mode( $tax_display_mode );
-
-		$price = array();
-
-		if ( $product->is_type( 'variable' ) && $product->has_child() ) {
-			$prices = $product->get_variation_prices( true );
-
-			if ( empty( $prices['price'] ) ) {
-				/**
-				 * Filter the variable products empty prices.
-				 *
-				 * @since 3.1.0 Introduced.
-				 *
-				 * @param array      $prices  Empty array.
-				 * @param WC_Product $product The project object.
-				 */
-				$price = apply_filters( 'cocart_products_variable_empty_price', array(), $product );
-			} else {
-				$min_price     = current( $prices['price'] );
-				$max_price     = end( $prices['price'] );
-				$min_reg_price = current( $prices['regular_price'] );
-				$max_reg_price = end( $prices['regular_price'] );
-
-				if ( $min_price !== $max_price ) {
-					$price = array(
-						'from' => cocart_prepare_money_response( $min_price, wc_get_price_decimals() ),
-						'to'   => cocart_prepare_money_response( $max_price, wc_get_price_decimals() ),
-					);
-				} else {
-					$price = array(
-						'from' => cocart_prepare_money_response( $min_price, wc_get_price_decimals() ),
-						'to'   => '',
-					);
-				}
-			}
-		}
-
-		if ( $product->is_type( 'grouped' ) ) {
-			$children       = array_filter( array_map( 'wc_get_product', $product->get_children() ), 'wc_products_array_filter_visible_grouped' );
-			$price_function = CoCart_Utilities_Product_Helpers::get_price_from_tax_display_mode( $tax_display_mode );
-
-			foreach ( $children as $child ) {
-				if ( '' !== $child->get_price() ) {
-					$child_prices[] = $price_function( $child );
-				}
-			}
-
-			if ( ! empty( $child_prices ) ) {
-				$price = array(
-					'from' => cocart_prepare_money_response( min( $child_prices ), wc_get_price_decimals() ),
-					'to'   => cocart_prepare_money_response( max( $child_prices ), wc_get_price_decimals() ),
-				);
-			}
-		}
-
-		/**
-		 * Filters the products price range.
-		 *
-		 * @since 3.1.0 Introduced.
-		 *
-		 * @param array      $price   The current product price range.
-		 * @param WC_Product $product The product object.
-		 */
-		return apply_filters( 'cocart_products_get_price_range', $price, $product );
+		return CoCart_Utilities_Product_Helpers::get_price_range( $product, $tax_display_mode );
 	} // END get_price_range()
 
 	/**
@@ -1069,46 +951,7 @@ class CoCart_REST_Products_V2_Controller extends CoCart_Products_Controller {
 	public function get_meta_data( $product ) {
 		cocart_deprecated_function( 'CoCart_REST_Products_V2_Controller::get_meta_data', '4.3.3', 'CoCart_Utilities_Product_Helpers::get_meta_data' );
 
-		$meta_data = $product->get_meta_data();
-		$safe_meta = array();
-
-		/**
-		 * Filter allows you to ignore private meta data for the product to return.
-		 *
-		 * When filtering, only list the meta key!
-		 *
-		 * @since 3.11.0 Introduced.
-		 *
-		 * @param array      $meta_keys List of meta keys.
-		 * @param WC_Product $product   The product object.
-		 */
-		$ignore_private_meta_keys = apply_filters( 'cocart_products_ignore_private_meta_keys', array(), $product );
-
-		foreach ( $meta_data as $meta ) {
-			$ignore_meta = false;
-
-			foreach ( $ignore_private_meta_keys as $ignore ) {
-				if ( str_starts_with( $meta->key, $ignore ) ) {
-					$ignore_meta = true;
-					break; // Exit the inner loop once a match is found.
-				}
-			}
-
-			// Add meta data only if it's not ignored.
-			if ( ! $ignore_meta ) {
-				$safe_meta[ $meta->key ] = $meta;
-			}
-		}
-
-		/**
-		 * Filter allows you to control what remaining product meta data is safe to return.
-		 *
-		 * @since 3.11.0 Introduced.
-		 *
-		 * @param array      $safe_meta Meta data.
-		 * @param WC_Product $product   The product object.
-		 */
-		return array_values( apply_filters( 'cocart_products_get_safe_meta_data', $safe_meta, $product ) );
+		return CoCart_Utilities_Product_Helpers::get_meta_data( $product );
 	} // END get_meta_data()
 
 	/**
