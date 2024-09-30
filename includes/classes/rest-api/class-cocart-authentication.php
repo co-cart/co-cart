5ddda7d6e65e54e8a62a0c1543610e96e637c7dc
Sébastien Dumont <mailme@sebastiendumont.com>
2024-09-30 18:14:02 +0200

    Revert added @since tag

--- includes/classes/rest-api/class-cocart-authentication.php
+++ includes/classes/rest-api/class-cocart-authentication.php
@@ -570,7 +570,6 @@ if ( ! class_exists( 'CoCart_Authentication' ) ) {
 		 *
 		 * @since 2.2.0 Introduced.
 		 * @since 3.3.0 Added new custom headers without the prefix `X-`
-		 * @since 4.4.0 Fallback to a wildcard if the origin has yet to be determined.
 		 *
 		 * @uses CoCart_Authentication()::get_http_origin()
 		 * @uses CoCart_Authentication()::is_allowed_http_origin()
