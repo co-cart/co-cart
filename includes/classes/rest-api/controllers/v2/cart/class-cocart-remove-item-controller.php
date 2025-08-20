961affeb51ad0be590c64471ae49cd506c8b5500
Sebastien <sebastiendumont1986@gmail.com>
2025-08-21 00:28:59 +0200

    Restrict item key to only 32 characters

--- includes/classes/rest-api/controllers/v2/cart/class-cocart-remove-item-controller.php
+++ includes/classes/rest-api/controllers/v2/cart/class-cocart-remove-item-controller.php
@@ -46,7 +46,7 @@ class CoCart_REST_Remove_Item_V2_Controller extends CoCart_REST_Cart_V2_Controll
 		// Remove Item - cocart/v2/cart/item/6364d3f0f495b6ab9dcf8d3b5c6e0b01 (DELETE).
 		register_rest_route(
 			$this->namespace,
-			'/' . $this->rest_base . '/(?P<item_key>[\w]+)',
+			'/' . $this->rest_base . '/(?P<item_key>[\w-]{32})',
 			array(
 				array(
 					'methods'             => WP_REST_Server::DELETABLE,
