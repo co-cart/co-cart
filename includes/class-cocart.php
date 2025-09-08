3f76d097bbac06db94f01164f74dcd6d2b73fe20
Sebastien <sebastiendumont1986@gmail.com>
2025-09-08 21:12:53 +0200

    Bumped version build

--- includes/class-cocart.php
+++ includes/class-cocart.php
@@ -28,7 +28,7 @@ final class CoCart {
 	 *
 	 * @var string
 	 */
-	public static $version = '4.7.0';
+	public static $version = '4.8.0-beta.1';
 
 	/**
 	 * CoCart Database Schema version.
--- load-package.php
+++ load-package.php
@@ -2,7 +2,7 @@
 /**
  * This file is designed to be used to load as package NOT a WP plugin!
  *
- * @version 4.7.0
+ * @version 4.8.0-beta.1
  * @package CoCart
  */
 
