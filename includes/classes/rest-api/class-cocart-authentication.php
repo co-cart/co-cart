ad0eb3da3cac81093207de525d5eb968bec2872a
Sebastien <sebastiendumont1986@gmail.com>
2026-03-04 17:39:17 +0100

    Fixed authentication not determining user in time when `rest_url_prefix` filter is used causing several cloned guest sessions with cart items.

--- includes/classes/rest-api/class-cocart-authentication.php
+++ includes/classes/rest-api/class-cocart-authentication.php
@@ -85,11 +85,6 @@ if ( ! class_exists( 'CoCart_Authentication' ) ) {
 		 * @ignore Function ignored when parsed into Code Reference.
 		 */
 		public function __construct() {
-			// Check that we are only authenticating for our API.
-			if ( ! CoCart::is_rest_api_request() ) {
-				return;
-			}
-
 			// Authenticate user.
 			add_filter( 'determine_current_user', array( $this, 'authenticate' ) );
 			add_filter( 'rest_authentication_errors', array( $this, 'authentication_fallback' ) );
@@ -275,8 +270,8 @@ if ( ! class_exists( 'CoCart_Authentication' ) ) {
 		 * @return int|false
 		 */
 		public function authenticate( $user_id ) {
-			// Do not authenticate twice.
-			if ( ! empty( $user_id ) ) {
+			// Do not authenticate twice and check if is a request to our endpoint.
+			if ( ! empty( $user_id ) || ! CoCart::is_rest_api_request() ) {
 				return $user_id;
 			}
 
