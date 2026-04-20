074fe66e8cdc7124d6a2c26c57bfd535beef4084
Sebastien <sebastiendumont1986@gmail.com>
2026-04-20 18:04:25 +0200

    Updated doc URL

--- includes/class-cocart.php
+++ includes/class-cocart.php
@@ -186,7 +186,7 @@ final class CoCart {
 		self::define( 'COCART_REVIEW_URL', 'https://testimonial.to/cocart' );
 		self::define( 'COCART_SUGGEST_FEATURE', 'https://cocartapi.com/suggest-a-feature/' );
 		self::define( 'COCART_COMMUNITY_URL', 'https://cocartapi.com/community/' );
-		self::define( 'COCART_DOCUMENTATION_URL', 'https://cocartapi.com/docs/' );
+		self::define( 'COCART_DOCUMENTATION_URL', 'https://docs.cocartapi.com/' );
 		self::define( 'COCART_TRANSLATION_URL', 'https://translate.cocartapi.com/projects/cart-rest-api-for-woocommerce/' );
 		self::define( 'COCART_REPO_URL', 'https://github.com/co-cart/co-cart' );
 		self::define( 'COCART_NEXT_VERSION', '5.0.0' );
