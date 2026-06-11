b49e16b1f56a11e2871391c7e3a22e117b443caa
Sebastien <sebastiendumont1986@gmail.com>
2026-06-11 19:22:26 +0200

    Bumped tested up to

--- cart-rest-api-for-woocommerce.php
+++ cart-rest-api-for-woocommerce.php
@@ -9,7 +9,7 @@
  * Text Domain: cart-rest-api-for-woocommerce
  * Domain Path: /languages/
  * Requires at least: 6.7
- * Tested up to: 6.9
+ * Tested up to: 7.0
  * Requires PHP: 8.2
  * Requires Plugins: woocommerce
  *
